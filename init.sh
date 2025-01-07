2ba1647623d504a19b703fbf19cfb6d161d5cf97
gszp1 <grzybekmateusz@poczta.fm>
2025-01-07 17:39:44 +0100

    Adds CSI.

--- init.sh
+++ init.sh
@@ -6,4 +6,6 @@ python3 ./scripts/get_ansible_hosts.py ./config/cluster_config.json
 ansible-playbook -i ./build/hosts.ini ./ansible/kubernetes-common.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/control-plane.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/worker.yml
+ansible-playbook -i ./build/hosts.ini ./ansible/nfs/nfs.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/nfs/csi.yaml
 # rm -rf ./tmp
