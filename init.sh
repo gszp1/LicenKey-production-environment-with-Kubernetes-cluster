32c7a32e00f364d2bba16492f2519b18afbba7ac
gszp1 <grzybekmateusz@poczta.fm>
2025-01-08 00:45:01 +0100

    Changes.

--- init.sh
+++ init.sh
@@ -3,11 +3,12 @@
 mkdir -p ./ansible/tmp
 vagrant up
 python3 ./scripts/get_ansible_hosts.py ./config/cluster_config.json
+ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # Moved to the start before cluster is created
 ansible-playbook -i ./build/hosts.ini ./ansible/kubernetes-common.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/control-plane.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/worker.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/nfs.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/csi.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml
+# ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # CHECK IF IT DOES NOT BREAK CLUSTER BY RESTARTING DAEMONS
 # rm -rf ./tmp
