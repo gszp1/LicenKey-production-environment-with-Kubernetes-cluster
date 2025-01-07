8c4112f7ff3119e18d80bba5b3555401ed67f342
gszp1 <grzybekmateusz@poczta.fm>
2025-01-08 00:54:58 +0100

    Moves setting insecure registry to kubernetes-common.yaml (Needs testing).

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 mkdir -p ./ansible/tmp
 vagrant up
 python3 ./scripts/get_ansible_hosts.py ./config/cluster_config.json
-ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # Moved to the start before cluster is created
+# ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # Moved to the start before cluster is created
 ansible-playbook -i ./build/hosts.ini ./ansible/kubernetes-common.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/control-plane.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/worker.yml
