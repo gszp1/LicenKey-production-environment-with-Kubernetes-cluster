cca64d7715c580f21ae3db3b10a614f6ca949880
gszp1 <grzybekmateusz@poczta.fm>
2025-01-07 22:56:46 +0100

    Creates docker registry.

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ python3 ./scripts/get_ansible_hosts.py ./config/cluster_config.json
 ansible-playbook -i ./build/hosts.ini ./ansible/kubernetes-common.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/control-plane.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/worker.yml
-ansible-playbook -i ./build/hosts.ini ./ansible/nfs/nfs.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/nfs/csi.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/nfs.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/csi.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
 # rm -rf ./tmp
