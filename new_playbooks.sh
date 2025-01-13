d9e4f8857ec067cd7be1e8144d8e341937bfdfad
gszp1 <grzybekmateusz@poczta.fm>
2025-01-13 15:17:04 +0100

    Adds playbook for ingress-nginx.

--- new_playbooks.sh
+++ new_playbooks.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # DO NOT ENTER ANY EXTRA SPAPCES, NEW LINES OR OTHER COMMANDS BETWEEN, AFTER OR BEFORE PLAYBOOK CALLS
 ansible-playbook -i ./build/hosts.ini ./ansible/cert-manager.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/nginx-ingress.yaml
