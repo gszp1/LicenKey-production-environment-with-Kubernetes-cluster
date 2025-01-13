4338a751dbae336b42fb17b785c5ab79a7c81189
gszp1 <grzybekmateusz@poczta.fm>
2025-01-13 11:23:32 +0100

    Installs Helm on Master Node.

--- init.sh
+++ init.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Create VMs
 mkdir -p ./ansible/tmp
 vagrant up
+
+# Create and configure k8s cluster on VMs
 python3 ./scripts/get_ansible_hosts.py ./config/cluster_config.json
-# ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # Moved to the start before cluster is created
 ansible-playbook -i ./build/hosts.ini ./ansible/kubernetes-common.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/control-plane.yml
 ansible-playbook -i ./build/hosts.ini ./ansible/worker.yml
+ansible-playbook -i ./build/hosts.ini ./ansible/helm.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/nfs.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/csi.yaml
+
+# Create k8s objects 
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
-# ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry-node-config.yaml # CHECK IF IT DOES NOT BREAK CLUSTER BY RESTARTING DAEMONS
+
 # rm -rf ./tmp
