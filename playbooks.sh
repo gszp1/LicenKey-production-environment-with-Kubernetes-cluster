8c4d15c7f9a18761a20afb178253323a401cd3ed
gszp1 <grzybekmateusz@poczta.fm>
2025-01-14 20:14:02 +0100

    Adds playbook for generating credentials with htpasswd.

--- playbooks.sh
+++ playbooks.sh
@@ -8,8 +8,9 @@ ansible-playbook -i ./build/hosts.ini ./ansible/helm.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/cert-manager.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/metallb/metallb.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/nginx-ingress.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/tls-nodes-config.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/registry-authentication.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/certificates.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/routing-ingress.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/share-certificate.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/docker-registry-routing/tls-nodes-config.yaml
