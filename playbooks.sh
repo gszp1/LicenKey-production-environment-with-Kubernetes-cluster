c244f4b93da5640b58624f5ba969ddb614c61d5c
gszp1 <grzybekmateusz@poczta.fm>
2025-01-14 16:30:43 +0100

    Moves certificates and ingress to separate namespace 'routing'.

--- playbooks.sh
+++ playbooks.sh
@@ -7,5 +7,8 @@ ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/csi.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/helm.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/cert-manager.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/metallb/metallb.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/nginx-ingress.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/routing/certificates.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/routing/registry-ingress.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/routing/share-certificate.yaml
