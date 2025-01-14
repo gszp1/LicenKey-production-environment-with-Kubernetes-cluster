1f820324733479a23d36d8c47138cfc16f94f213
gszp1 <grzybekmateusz@poczta.fm>
2025-01-14 01:39:23 +0100

    Merges new playbooks.

--- init.sh
+++ init.sh
@@ -16,3 +16,6 @@ ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/nfs.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/csi.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/helm.yaml
 ansible-playbook -i ./build/hosts.ini ./ansible/docker_registry/docker-registry.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/cert-manager.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/metallb/metallb.yaml
+ansible-playbook -i ./build/hosts.ini ./ansible/nginx-ingress.yaml
--- new_playbooks.sh
+++ new_playbooks.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 # DO NOT ENTER ANY EXTRA SPAPCES, NEW LINES OR OTHER COMMANDS BETWEEN, AFTER OR BEFORE PLAYBOOK CALLS
-ansible-playbook -i ./build/hosts.ini ./ansible/cert-manager.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/metallb/metallb.yaml
-ansible-playbook -i ./build/hosts.ini ./ansible/nginx-ingress.yaml
